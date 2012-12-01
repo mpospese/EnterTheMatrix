a242ed9bb98c2bda73b9b23be57689f7f7a2135b
Mark Pospesel <mpospese@gmail.com>
2012-11-30 21:43:20 -0500

    Source code cleanup - keep private properties and methods private.

--- EnterTheMatrix/BasicAnimationViewController.m
+++ EnterTheMatrix/BasicAnimationViewController.m
@@ -13,6 +13,24 @@
 
 @interface BasicAnimationViewController ()
 
+@property (weak, nonatomic) IBOutlet UISegmentedControl *modeSegment;
+@property (weak, nonatomic) IBOutlet UISwitch *speedSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *translateSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *scaleSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *rotateSwitch;
+@property (weak, nonatomic) IBOutlet UIView *bar;
+@property (weak, nonatomic) IBOutlet UILabel *translateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scaleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rotateLabel;
+@property (weak, nonatomic) IBOutlet UIView *controlFrame;
+
+@property (assign, nonatomic) OrientationMode orientation;
+@property (readonly, nonatomic) BOOL isFast;
+@property (readonly, nonatomic) BOOL useTranslate;
+@property (readonly, nonatomic) BOOL useScale;
+@property (readonly, nonatomic) BOOL useRotate;
+@property (readonly, nonatomic) AnimationMode animation;
+
 @property (nonatomic, assign, getter = isRotating) BOOL rotating;
 
 @end
